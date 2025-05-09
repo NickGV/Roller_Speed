5793991c76fe959cbb9699a205c94441d3f74757
ropall <cristian.pallares@est.iudigital.edu.co>
2025-05-09 05:31:08 -0500

    feat: Initialize Spring Boot project with basic structure and configurations

---
+++ speed_roller/src/main/java/com/speedroller/speed_roller/SpeedRollerApplication.java
@@ -0,0 +1,14 @@
+package com.speedroller.speed_roller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpeedRollerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpeedRollerApplication.class, args);
+		System.out.println("Aplicacion conectada Correctamente");
+	}
+
+}
