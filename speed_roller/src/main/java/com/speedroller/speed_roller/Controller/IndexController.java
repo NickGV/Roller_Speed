58bb05f357ad16734dd2642575880e895de303cc
Josenisperuza123 <jose.nisperuza@est.iudigital.edu.co>
2025-05-19 12:51:13 -0500

    pvm pages completed

--- speed_roller/src/main/java/com/speedroller/speed_roller/Controller/IndexController.java
+++ speed_roller/src/main/java/com/speedroller/speed_roller/Controller/IndexController.java
@@ -1,8 +1,11 @@
 package com.speedroller.speed_roller.Controller;
+import java.util.Arrays;
+import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
 //import org.springframework.web.bind.annotation.RequestParam;
 
 
@@ -33,6 +36,37 @@ public class IndexController {
     @GetMapping("eventos")
     public String eventos() {
         return "eventos";
+    }
+     @GetMapping("/registro/estudiante")
+    public String inscribirEstudiante() {
+        return "registro/inscribir-estudiante";
+    }
+
+    @GetMapping("/registro/instructor")
+    public String inscribirInstructor() {
+        return "registro/inscribir-instructor";
+    }
+
+    @GetMapping("/registro/clases")
+    public String registroClases() {
+        return "registro/clases";
+    }
+
+    @GetMapping("/estudiantes/listar")
+    public String listarEstudiantes(Model model) {
+        List<String> estudiantes = Arrays.asList("Ana Torres", "Luis Gómez", "Carlos Rojas");
+        model.addAttribute("estudiantes", estudiantes);
+        return "estudiantes/listar-estudiantes";
+    }
+
+    @GetMapping("/calendario/estudiantes")
+    public String horarioEstudiantes() {
+        return "calendario/horario-estudiantes";
+    }
+
+    @GetMapping("/calendario/instructores")
+    public String horarioInstructores() {
+        return "calendario/horario-instructores";
     }
     
 }
