36bf48d95f464f955a3f3241d184e05b8a711cb5
Nick <nickgomvelez@gmail.com>
2025-05-20 09:57:11 -0500

    Refactor CorporativoController and EventosController, remove ServiciosController, and update navbar links for improved routing and code clarity.

--- speed_roller/src/main/java/com/speedroller/speed_roller/Controller/CorporativoController.java
+++ speed_roller/src/main/java/com/speedroller/speed_roller/Controller/CorporativoController.java
@@ -1,4 +1,5 @@
 package com.speedroller.speed_roller.Controller;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,5 +21,10 @@ public class CorporativoController {
   @GetMapping("/valores")
   public String valores() {
     return "valores";
+  }
+
+  @GetMapping("/servicios")
+  public String servicios() {
+    return "servicios";
   }
 }
