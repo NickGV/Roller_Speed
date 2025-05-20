14446b64247d77b2f812725ee4e14e6734155ebe
Nick <nickgomvelez@gmail.com>
2025-05-20 09:07:09 -0500

    Add Eventos and Servicios controllers, update servicios.html to use navbar and footer fragments, and enhance styles for event images

---
+++ speed_roller/src/main/java/com/speedroller/speed_roller/Controller/EventosController.java
@@ -0,0 +1,14 @@
+package com.speedroller.speed_roller.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Controller
+public class EventosController {
+    
+    @GetMapping("/eventos")
+    public String eventos() {
+        return "eventos";
+    }
+}
---
+++ speed_roller/src/main/java/com/speedroller/speed_roller/Controller/ServiciosController.java
@@ -0,0 +1,13 @@
+package com.speedroller.speed_roller.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ServiciosController {
+
+  @GetMapping("/servicios")
+  public String servicios() {
+    return "servicios";
+  }
+}
