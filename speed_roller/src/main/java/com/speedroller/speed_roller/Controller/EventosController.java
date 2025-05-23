0901625f3fe877d3eca3d7b110d6a7c3aecb7195
ropall <cristian.pallares@est.iudigital.edu.co>
2025-05-22 21:58:51 -0500

    Add request mapping for eventos in EventosController

--- speed_roller/src/main/java/com/speedroller/speed_roller/Controller/EventosController.java
+++ speed_roller/src/main/java/com/speedroller/speed_roller/Controller/EventosController.java
@@ -2,9 +2,12 @@ package com.speedroller.speed_roller.Controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 
 @Controller
+@RequestMapping("/eventos")
+// Controlador para la página de eventos
 public class EventosController {
     @GetMapping("/eventos")
     public String eventos() {
