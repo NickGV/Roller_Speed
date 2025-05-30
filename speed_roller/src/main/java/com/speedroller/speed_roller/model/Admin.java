446f1f4312c9ee3a2840d5cde0dca77c35e2b2a4
ropall <cristian.pallares@est.iudigital.edu.co>
2025-05-29 21:55:33 -0500

    Refactor Admin model: add JPA annotations and set default role to "ADMINISTRADOR"

--- speed_roller/src/main/java/com/speedroller/speed_roller/model/Admin.java
+++ speed_roller/src/main/java/com/speedroller/speed_roller/model/Admin.java
@@ -1,5 +1,11 @@
 package com.speedroller.speed_roller.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -7,12 +13,24 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @Data
 @NoArgsConstructor
+@Entity
+@Table(name = "tbl_admins")
 public class Admin {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+
+    @Column(nullable = false, length = 50)
     private String username;
+
+    @Column(nullable = false, length = 255)
     private String password;
+    
+    @Column(nullable = false, unique = true, length = 100) //Con unique= true evitamos que se repita el email
     private String email;
-    private String role;
+
+    @Column(nullable = false, length = 50)
+    private String role= "ADMINISTRADOR"; // ROL POR DEFECTO
 
 }
